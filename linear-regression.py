f096daa20aee96ee949b89c3e71f4b7ca994928c
atique.ahmad@formsoft-inc.com <65412984+Atique22@users.noreply.github.com>
2023-07-04 14:51:23 +0500

    linear regression

--- linear-regression.py
+++ linear-regression.py
@@ -1,14 +1,33 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
 #  load dataset 
 data = pd.read_csv("house_data.csv")
 
 print(data.head())
 
 
-# Create a scatter plot
-plt.scatter(data['Size'], data['Price'])
-plt.xlabel('Size (in square feet)')
-plt.ylabel('Price (in dollars)')
-plt.title('House Prices')
+
+
+# Separate the features (X) and target variable (y)
+X = data[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long', 'sqft_living15', 'sqft_lot15']]
+y = data['price']
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Generate predictions
+y_pred = model.predict(X)
+
+# Plot the data points
+plt.scatter(y, y_pred)
+plt.xlabel('Actual Price')
+plt.ylabel('Predicted Price')
+
+# Plot the best fit line
+plt.plot([y.min(), y.max()], [y.min(), y.max()], 'r--')
 plt.show()
